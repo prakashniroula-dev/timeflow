ccfcb61e855647bd9f9e296817bac570a229cb85
Prakash Niroula <prakashniroula.dev@gmail.com>
2025-12-19 21:07:09 +0545

    timeflow i/o headers

---
+++ src/include/tf_io.h
@@ -0,0 +1,11 @@
+#if !defined(TIMEFLOW_IO_H)
+#define TIMEFLOW_IO_H
+
+#include <stdio.h>
+
+#define tf_print_err(...) fprintf(stderr, "[tf error]: " __VA_ARGS__)
+#define tf_print_warn(...) fprintf(stderr, "[tf warn]: " __VA_ARGS__)
+#define tf_log(...) fprintf(stdin, "[tf log]: " __VA_ARGS__)
+#define tf_print printf
+
+#endif // TIMEFLOW_IO_H
