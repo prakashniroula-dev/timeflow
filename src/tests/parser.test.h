4fd6f395c9208842b11f77f8f6529efbbe4b16a0
Prakash Niroula <prakashniroula.dev@gmail.com>
2025-12-19 21:07:32 +0545

    update tests

--- src/tests/parser.test.h
+++ src/tests/parser.test.h
@@ -8,21 +8,12 @@
 /* Development phase */
 
 #include <parser.h>
-#include <test.h>
 #include <stdbool.h>
 #include <stdio.h>
 
 static inline void test_parser() {
-  tf_time t;
-
-  puts("");
-  
-  printf("%d\n", parse_time("4:00", &t));
-  printf("%d:%02d\n", t.start.clock.h, t.start.clock.m);
-  printf("%d:%02d\n\n", t.end.clock.h, t.end.clock.m);
-
-  printf("%d\n", parse_time("04:00 - 08:12", &t));
-  printf("%d:%02d\n", t.start.clock.h, t.start.clock.m);
-  printf("%d:%02d\n\n", t.end.clock.h, t.end.clock.m);
-  
+  const char* str = "25:45";
+  const char* ptr = str;
+  struct tf_atm t = tfp_parse_time_atm(&ptr, NULL);
+  printf("%d:%02d", t.clock.h, t.clock.m);
 }
