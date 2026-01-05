e00a2eb3473e401ea8acb5809ebcf5f6181146bd
Prakash Niroula <prakashniroula.dev@gmail.com>
2026-01-05 22:43:05 +0545

    small macro fix

--- src/include/tf_io.h
+++ src/include/tf_io.h
@@ -6,6 +6,6 @@
 #define tf_print_err(...) fprintf(stderr, "[tf error]: " __VA_ARGS__)
 #define tf_print_warn(...) fprintf(stderr, "[tf warn]: " __VA_ARGS__)
 #define tf_log(...) fprintf(stdin, "[tf log]: " __VA_ARGS__)
-#define tf_print printf
+#define tf_print(...) fprintf(stdout, "[tf]: " __VA_ARGS__)
 
 #endif // TIMEFLOW_IO_H
