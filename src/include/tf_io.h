ddd73095cbe307cd9c728edd2f324838d25928fd
Prakash Niroula <prakashniroula.dev@gmail.com>
2026-01-06 12:42:31 +0545

    fix io changes

--- src/include/tf_io.h
+++ src/include/tf_io.h
@@ -5,7 +5,7 @@
 
 #define tf_print_err(...) fprintf(stderr, "[tf error]: " __VA_ARGS__)
 #define tf_print_warn(...) fprintf(stderr, "[tf warn]: " __VA_ARGS__)
-#define tf_log(...) fprintf(stdin, "[tf log]: " __VA_ARGS__)
+#define tf_log(...) fprintf(stderr, "[tf log]: " __VA_ARGS__)
 #define tf_print(...) fprintf(stdout, "[tf]: " __VA_ARGS__)
 
 #endif // TIMEFLOW_IO_H
